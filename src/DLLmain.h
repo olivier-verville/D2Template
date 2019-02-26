0c6c8921ee2f48b223a5cd70c0aaa453eced3f39
devurandom96 <33327577+devurandom96@users.noreply.github.com>
2019-02-25 22:45:51 -0700

    Reduce Dll footprint 30-80% - Add GetDllOffset for Ordinals
    Eliminates duplicate pointers for #include "D2Ptrs.h" to all translational units.
    Eliminates duplicates module def's for #include "DLLmain.h" to all translational units.
    Both need /OPT:ICF linker option (COMDAT FOLDING) to get reduction in size from.
    Update def's for GCC
    Add GetDllOffset for Ordinals

--- src/DLLmain.h
+++ src/DLLmain.h
@@ -30,28 +30,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static const DWORD DLLBASE_BNCLIENT		=	(DWORD)LoadLibraryA("Bnclient.dll");
-static const DWORD DLLBASE_D2CLIENT		=	(DWORD)LoadLibraryA("D2Client.dll");
-static const DWORD DLLBASE_D2CMP		=	(DWORD)LoadLibraryA("D2CMP.dll");
-static const DWORD DLLBASE_D2COMMON		=	(DWORD)LoadLibraryA("D2Common.dll");
-static const DWORD DLLBASE_D2DDRAW		=	(DWORD)LoadLibraryA("D2DDraw.dll");
-static const DWORD DLLBASE_D2DIRECT3D	=	(DWORD)LoadLibraryA("D2Direct3D.dll");
-static const DWORD DLLBASE_D2GAME		=	(DWORD)LoadLibraryA("D2Game.dll");
-static const DWORD DLLBASE_D2GDI		=	(DWORD)LoadLibraryA("D2Gdi.dll");
-static const DWORD DLLBASE_D2GFX		=	(DWORD)LoadLibraryA("D2Gfx.dll");
-static const DWORD DLLBASE_D2GLIDE		=	(DWORD)LoadLibraryA("D2Glide.dll");
-static const DWORD DLLBASE_D2LANG		=	(DWORD)LoadLibraryA("D2Lang.dll");
-static const DWORD DLLBASE_D2LAUNCH		=	(DWORD)LoadLibraryA("D2Launch.dll");
-static const DWORD DLLBASE_D2MCPCLIENT	=	(DWORD)LoadLibraryA("D2MCPClient.dll");
-static const DWORD DLLBASE_D2MULTI		=	(DWORD)LoadLibraryA("D2Multi.dll");
-static const DWORD DLLBASE_D2NET		=	(DWORD)LoadLibraryA("D2Net.dll");
-static const DWORD DLLBASE_D2SOUND		=	(DWORD)LoadLibraryA("D2Sound.dll");
-static const DWORD DLLBASE_D2WIN		=	(DWORD)LoadLibraryA("D2Win.dll");
-static const DWORD DLLBASE_FOG			=	(DWORD)LoadLibraryA("Fog.dll");
-static const DWORD DLLBASE_STORM		=	(DWORD)LoadLibraryA("Storm.dll");
-static const DWORD DLLBASE_IJL11		=	(DWORD)LoadLibraryA("ijl11.dll");
-static const DWORD DLLBASE_BINKW32		=	(DWORD)LoadLibraryA("binkw32.dll");
-static const DWORD DLLBASE_SMACKW32		=	(DWORD)LoadLibraryA("SmackW32.dll");
+
+#ifdef _MSC_VER // MS Compiler def's
+__declspec(selectany) extern const DWORD DLLBASE_BNCLIENT     =   (DWORD)LoadLibraryA("Bnclient.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2CLIENT     =   (DWORD)LoadLibraryA("D2Client.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2CMP        =   (DWORD)LoadLibraryA("D2CMP.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2COMMON     =   (DWORD)LoadLibraryA("D2Common.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2DDRAW      =   (DWORD)LoadLibraryA("D2DDraw.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2DIRECT3D   =   (DWORD)LoadLibraryA("D2Direct3D.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2GAME       =   (DWORD)LoadLibraryA("D2Game.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2GDI        =   (DWORD)LoadLibraryA("D2Gdi.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2GFX        =   (DWORD)LoadLibraryA("D2Gfx.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2GLIDE      =   (DWORD)LoadLibraryA("D2Glide.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2LANG       =   (DWORD)LoadLibraryA("D2Lang.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2LAUNCH     =   (DWORD)LoadLibraryA("D2Launch.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2MCPCLIENT  =   (DWORD)LoadLibraryA("D2MCPClient.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2MULTI      =   (DWORD)LoadLibraryA("D2Multi.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2NET        =   (DWORD)LoadLibraryA("D2Net.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2SOUND      =   (DWORD)LoadLibraryA("D2Sound.dll");
+__declspec(selectany) extern const DWORD DLLBASE_D2WIN        =   (DWORD)LoadLibraryA("D2Win.dll");
+__declspec(selectany) extern const DWORD DLLBASE_FOG          =   (DWORD)LoadLibraryA("Fog.dll");
+__declspec(selectany) extern const DWORD DLLBASE_STORM        =   (DWORD)LoadLibraryA("Storm.dll");
+__declspec(selectany) extern const DWORD DLLBASE_IJL11        =   (DWORD)LoadLibraryA("ijl11.dll");
+__declspec(selectany) extern const DWORD DLLBASE_BINKW32      =   (DWORD)LoadLibraryA("binkw32.dll");
+__declspec(selectany) extern const DWORD DLLBASE_SMACKW32     =   (DWORD)LoadLibraryA("SmackW32.dll");
+#else //GCC Compiler def's
+extern const DWORD DLLBASE_BNCLIENT __attribute__((weak))     =   (DWORD)LoadLibraryA("Bnclient.dll");
+extern const DWORD DLLBASE_D2CLIENT __attribute__((weak))     =   (DWORD)LoadLibraryA("D2Client.dll");
+extern const DWORD DLLBASE_D2CMP __attribute__((weak))        =   (DWORD)LoadLibraryA("D2CMP.dll");
+extern const DWORD DLLBASE_D2COMMON __attribute__((weak))     =   (DWORD)LoadLibraryA("D2Common.dll");
+extern const DWORD DLLBASE_D2DDRAW __attribute__((weak))      =   (DWORD)LoadLibraryA("D2DDraw.dll");
+extern const DWORD DLLBASE_D2DIRECT3D __attribute__((weak))   =   (DWORD)LoadLibraryA("D2Direct3D.dll");
+extern const DWORD DLLBASE_D2GAME __attribute__((weak))       =   (DWORD)LoadLibraryA("D2Game.dll");
+extern const DWORD DLLBASE_D2GDI __attribute__((weak))        =   (DWORD)LoadLibraryA("D2Gdi.dll");
+extern const DWORD DLLBASE_D2GFX __attribute__((weak))        =   (DWORD)LoadLibraryA("D2Gfx.dll");
+extern const DWORD DLLBASE_D2GLIDE __attribute__((weak))      =   (DWORD)LoadLibraryA("D2Glide.dll");
+extern const DWORD DLLBASE_D2LANG __attribute__((weak))       =   (DWORD)LoadLibraryA("D2Lang.dll");
+extern const DWORD DLLBASE_D2LAUNCH  __attribute__((weak))    =   (DWORD)LoadLibraryA("D2Launch.dll");
+extern const DWORD DLLBASE_D2MCPCLIENT __attribute__((weak))  =   (DWORD)LoadLibraryA("D2MCPClient.dll");
+extern const DWORD DLLBASE_D2MULTI __attribute__((weak))      =   (DWORD)LoadLibraryA("D2Multi.dll");
+extern const DWORD DLLBASE_D2NET __attribute__((weak))        =   (DWORD)LoadLibraryA("D2Net.dll");
+extern const DWORD DLLBASE_D2SOUND __attribute__((weak))      =   (DWORD)LoadLibraryA("D2Sound.dll");
+extern const DWORD DLLBASE_D2WIN __attribute__((weak))        =   (DWORD)LoadLibraryA("D2Win.dll");
+extern const DWORD DLLBASE_FOG __attribute__((weak))          =   (DWORD)LoadLibraryA("Fog.dll");
+extern const DWORD DLLBASE_STORM __attribute__((weak))        =   (DWORD)LoadLibraryA("Storm.dll");
+extern const DWORD DLLBASE_IJL11 __attribute__((weak))        =   (DWORD)LoadLibraryA("ijl11.dll");
+extern const DWORD DLLBASE_BINKW32 __attribute__((weak))      =   (DWORD)LoadLibraryA("binkw32.dll");
+extern const DWORD DLLBASE_SMACKW32 __attribute__((weak))     =   (DWORD)LoadLibraryA("SmackW32.dll");
+#endif
 
 #include "D2Constants.h"
 #include "D2Structs.h"
